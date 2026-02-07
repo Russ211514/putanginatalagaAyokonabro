5f0043ffe384bd7ef22e99c4bc6350c0d6b34a9c
Russ211514 <russianjelomaster@gmail.com>
2026-02-07 18:39:58 +0800

    final level start

--- Scripts/final_level_html.gd
+++ Scripts/final_level_html.gd
@@ -1,31 +1,18 @@
 extends Control
 
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
-@onready var explanation: Panel = $CanvasLayer/Explanation
 @onready var question: Label = $CanvasLayer/Question
-@onready var understand: Button = $CanvasLayer/Understand
 @onready var next: Button = $CanvasLayer/Next
 
 func _ready() -> void:
 	canvas_layer.visible = false
 	if $Control:
 		$Control.visible = true
-	explanation.visible = false
 	question.visible = true
-	understand.visible = true
 	next.visible = true
 
-func _on_understand_pressed() -> void:
-	explanation.visible = true
-	question.visible = false
-	understand.visible = false
-	next.visible = false
-
-func _on_x_button_pressed() -> void:
-	_ready()
-
 func _on_next_pressed() -> void:
-	get_tree().change_scene_to_file("res://Html Scenes/html quiz part3.tscn")
+	pass
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Html Scenes/html_level_selector.tscn")
